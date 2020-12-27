259c94c12e6438f0b45038d0d3437779ce11a776
dennisppaul <dennis.paul@hfk-bremen.de>
2020-12-27 10:23:07 +0100

    updated build script and reference

--- dist/copy_reference.sh
+++ dist/copy_reference.sh
@@ -3,7 +3,16 @@
 LIB_NAME=$1
 ROOT=$(pwd)
 
+# update stylessheet from lib
+CSS=stylesheet.css
+SRC=$ROOT/../lib/$CSS
+DST=$ROOT/../reference
+cp "$SRC" "$DST"
+
+# copy reference
+
 SRC=$ROOT/../reference
 DST=$ROOT/../processing-library/$LIB_NAME/
 
 cp -r "$SRC" "$DST"
+
