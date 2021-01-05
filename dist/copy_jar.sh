898d1262deccfc85e532c8c7c9c0f67b385ce591
dennisppaul <dennis.paul@hfk-bremen.de>
2021-01-05 20:09:02 +0100

    removed `tests` and `examples` from jar

--- dist/copy_jar.sh
+++ dist/copy_jar.sh
@@ -6,12 +6,12 @@ ROOT=$(pwd)
 SRC=$ROOT/../lib/$LIB_NAME.jar
 DST=$ROOT/../processing-library/$LIB_NAME/library/
 
+# delete unwanted directories from jar 
+zip --quiet --delete $SRC wellen_SAM.h wellen_SAM_impl.h wellen_SAM_impl.cpp "wellen/tests/*" "wellen/examples/*"
+
 if [ -d "$DST" ]; then
 	rm -rf "$DST"
 fi
 mkdir -p "$DST"
 
 cp "$SRC" "$DST"
-
-# delete unwanted directories from jar 
-zip --quiet --delete $DST/$LIB_NAME.jar wellen_SAM.h wellen_SAM_impl.h wellen_SAM_impl.cpp "wellen/tests/*" "wellen/examples/*"
