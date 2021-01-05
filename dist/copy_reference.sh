d43cf8a073efe70f2629f32c4c7aa223e0d6bea5
dennisppaul <dennis.paul@hfk-bremen.de>
2021-01-05 13:13:24 +0100

    moved reference to docs

--- dist/copy_reference.sh
+++ dist/copy_reference.sh
@@ -6,12 +6,12 @@ ROOT=$(pwd)
 # update stylessheet from lib
 CSS=stylesheet.css
 SRC=$ROOT/../lib/$CSS
-DST=$ROOT/../reference
+DST=$ROOT/../docs/reference
 cp "$SRC" "$DST"
 
 # copy reference
 
-SRC=$ROOT/../reference
+SRC=$ROOT/../docs/reference
 DST=$ROOT/../processing-library/$LIB_NAME/
 
 cp -r "$SRC" "$DST"
