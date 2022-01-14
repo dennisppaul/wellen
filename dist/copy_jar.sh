1bc8cdc0a3c9c5d8602b01e007d51894b7738417
dennisppaul <dennis.paul@hfk-bremen.de>
2022-01-14 13:27:14 +0100

    removed jar cleaning

--- dist/copy_jar.sh
+++ dist/copy_jar.sh
@@ -7,7 +7,7 @@ SRC=$ROOT/../lib/$LIB_NAME.jar
 DST=$ROOT/../processing-library/$LIB_NAME/library/
 
 # delete unwanted directories from jar 
-zip --quiet --delete $SRC wellen_SAM.h wellen_SAM_impl.h wellen_SAM_impl.cpp "wellen/tests/*" "wellen/examples/*"
+#zip --quiet --delete $SRC wellen_SAM.h wellen_SAM_impl.h wellen_SAM_impl.cpp "wellen/tests/*" "wellen/examples/*"
 
 if [ -d "$DST" ]; then
 	rm -rf "$DST"
