05f3d3fc0dd557665e92bea4924640d6f6500dfd
dennisppaul <dennis.paul@hfk-bremen.de>
2022-05-24 13:52:26 +0200

    removed static imports from singing SAM example

--- processing-library/wellen/examples/external/ExampleExternal08SAMSinging/ExampleExternal08SAMSinging.pde
+++ processing-library/wellen/examples/external/ExampleExternal08SAMSinging/ExampleExternal08SAMSinging.pde
@@ -18,38 +18,38 @@ void setup() {
     mSAM = new SAM();
     mSAM.set_sing_mode(true);
     mWords = new TextFragment[]{
-    new TextFragment("EHEHEHEH", NOTE_C3),
-    new TextFragment("    ", NOTE_C3),
-    new TextFragment("VERIY", NOTE_D3),
-    new TextFragment("     ", NOTE_D3),
-    new TextFragment("TAYM", NOTE_D3 + 1),
-    new TextFragment("AY", NOTE_F3),
-    new TextFragment("  ", NOTE_F3),
-    new TextFragment("SIYIY", NOTE_G3),
-    new TextFragment("     ", NOTE_G3),
-    new TextFragment("YUW", NOTE_F3),
-    new TextFragment("   ", NOTE_F3),
-    new TextFragment("FAOAOAOAO", NOTE_D3 + 1),
-    new TextFragment("   ", NOTE_D3 + 1),
-    new TextFragment("LIHNX", NOTE_F3),
-    new TextFragment("     ", NOTE_F3),
-    new TextFragment("AY", NOTE_C3),
-    new TextFragment("GEHT", NOTE_A3 + 1),
-    new TextFragment("    ", NOTE_A3 + 1),
-    new TextFragment("DAWN", NOTE_G3 + 1),
-    new TextFragment("    ", NOTE_G3 + 1),
-    new TextFragment("AAN", NOTE_G3),
-    new TextFragment("MAY", NOTE_F3),
-    new TextFragment("   ", NOTE_F3),
-    new TextFragment("NIYZ", NOTE_D3 + 1),
-    new TextFragment("    ", NOTE_D3 + 1),
-    new TextFragment("AEND", NOTE_F3),
-    new TextFragment("    ", NOTE_F3),
-    new TextFragment("PREY", NOTE_C3),
-    new TextFragment("    ", NOTE_C3),
-    new TextFragment("", NOTE_C3),
-    new TextFragment("", NOTE_C3),
-    new TextFragment("", NOTE_C3),
+    new TextFragment("EHEHEHEH", Note.NOTE_C3),
+    new TextFragment("    ", Note.NOTE_C3),
+    new TextFragment("VERIY", Note.NOTE_D3),
+    new TextFragment("     ", Note.NOTE_D3),
+    new TextFragment("TAYM", Note.NOTE_D3 + 1),
+    new TextFragment("AY", Note.NOTE_F3),
+    new TextFragment("  ", Note.NOTE_F3),
+    new TextFragment("SIYIY", Note.NOTE_G3),
+    new TextFragment("     ", Note.NOTE_G3),
+    new TextFragment("YUW", Note.NOTE_F3),
+    new TextFragment("   ", Note.NOTE_F3),
+    new TextFragment("FAOAOAOAO", Note.NOTE_D3 + 1),
+    new TextFragment("   ", Note.NOTE_D3 + 1),
+    new TextFragment("LIHNX", Note.NOTE_F3),
+    new TextFragment("     ", Note.NOTE_F3),
+    new TextFragment("AY", Note.NOTE_C3),
+    new TextFragment("GEHT", Note.NOTE_A3 + 1),
+    new TextFragment("    ", Note.NOTE_A3 + 1),
+    new TextFragment("DAWN", Note.NOTE_G3 + 1),
+    new TextFragment("    ", Note.NOTE_G3 + 1),
+    new TextFragment("AAN", Note.NOTE_G3),
+    new TextFragment("MAY", Note.NOTE_F3),
+    new TextFragment("   ", Note.NOTE_F3),
+    new TextFragment("NIYZ", Note.NOTE_D3 + 1),
+    new TextFragment("    ", Note.NOTE_D3 + 1),
+    new TextFragment("AEND", Note.NOTE_F3),
+    new TextFragment("    ", Note.NOTE_F3),
+    new TextFragment("PREY", Note.NOTE_C3),
+    new TextFragment("    ", Note.NOTE_C3),
+    new TextFragment("", Note.NOTE_C3),
+    new TextFragment("", Note.NOTE_C3),
+    new TextFragment("", Note.NOTE_C3),
     };
     // | 1     |       | 2     |       | 3     |       | 4     |       |
     // | EH----------- | VERIY-------- | TAYM- | AY ---------- | SIY----
@@ -81,7 +81,16 @@ void beat(int pBeatCount) {
     int mWordIndex = pBeatCount % mWords.length;
     mSAM.set_pitch(SAM.get_pitch_from_MIDI_note(mWords[mWordIndex].pitch));
     mSAM.say(mWords[mWordIndex].text, true);
-    Tone.note_on(pBeatCount % 2 == 0 ? NOTE_C2 : NOTE_C3, 50);
+    Tone.note_on(pBeatCount % 2 == 0 ? Note.NOTE_C2 : Note.NOTE_C3, 50);
+}
+
+void mouseMoved() {
+    mSAM.set_mouth((int) map(mouseX, 0, width, 0, 255));
+    mSAM.set_throat((int) map(mouseY, 0, height, 0, 255));
+}
+
+void mouseDragged() {
+    mSAM.set_speed((int) map(mouseX, 0, width, 0, 255));
 }
 
 void audioblock(float[] pOutputSignal) {
--- src/wellen/examples/external/ExampleExternal08SAMSinging.java
+++ src/wellen/examples/external/ExampleExternal08SAMSinging.java
@@ -3,15 +3,9 @@ package wellen.examples.external;
 import processing.core.PApplet;
 import wellen.Beat;
 import wellen.DSP;
+import wellen.Note;
 import wellen.SAM;
 import wellen.Tone;
-
-import static wellen.MIDI.NOTE_A3;
-import static wellen.MIDI.NOTE_C2;
-import static wellen.MIDI.NOTE_C3;
-import static wellen.MIDI.NOTE_D3;
-import static wellen.MIDI.NOTE_F3;
-import static wellen.MIDI.NOTE_G3;
 
 public class ExampleExternal08SAMSinging extends PApplet {
 
@@ -33,39 +27,39 @@ public class ExampleExternal08SAMSinging extends PApplet {
         mSAM.set_sing_mode(true);
 
         mWords = new TextFragment[]{
-        new TextFragment("EHEHEHEH", NOTE_C3),
-        new TextFragment("    ", NOTE_C3),
-        new TextFragment("VERIY", NOTE_D3),
-        new TextFragment("     ", NOTE_D3),
-        new TextFragment("TAYM", NOTE_D3 + 1),
-        new TextFragment("AY", NOTE_F3),
-        new TextFragment("  ", NOTE_F3),
-        new TextFragment("SIYIY", NOTE_G3),
-        new TextFragment("     ", NOTE_G3),
-        new TextFragment("YUW", NOTE_F3),
-        new TextFragment("   ", NOTE_F3),
-        new TextFragment("FAOAOAOAO", NOTE_D3 + 1),
-        new TextFragment("   ", NOTE_D3 + 1),
-        new TextFragment("LIHNX", NOTE_F3),
-        new TextFragment("     ", NOTE_F3),
-        new TextFragment("AY", NOTE_C3),
+        new TextFragment("EHEHEHEH", Note.NOTE_C3),
+        new TextFragment("    ", Note.NOTE_C3),
+        new TextFragment("VERIY", Note.NOTE_D3),
+        new TextFragment("     ", Note.NOTE_D3),
+        new TextFragment("TAYM", Note.NOTE_D3 + 1),
+        new TextFragment("AY", Note.NOTE_F3),
+        new TextFragment("  ", Note.NOTE_F3),
+        new TextFragment("SIYIY", Note.NOTE_G3),
+        new TextFragment("     ", Note.NOTE_G3),
+        new TextFragment("YUW", Note.NOTE_F3),
+        new TextFragment("   ", Note.NOTE_F3),
+        new TextFragment("FAOAOAOAO", Note.NOTE_D3 + 1),
+        new TextFragment("   ", Note.NOTE_D3 + 1),
+        new TextFragment("LIHNX", Note.NOTE_F3),
+        new TextFragment("     ", Note.NOTE_F3),
+        new TextFragment("AY", Note.NOTE_C3),
 
-        new TextFragment("GEHT", NOTE_A3 + 1),
-        new TextFragment("    ", NOTE_A3 + 1),
-        new TextFragment("DAWN", NOTE_G3 + 1),
-        new TextFragment("    ", NOTE_G3 + 1),
-        new TextFragment("AAN", NOTE_G3),
-        new TextFragment("MAY", NOTE_F3),
-        new TextFragment("   ", NOTE_F3),
-        new TextFragment("NIYZ", NOTE_D3 + 1),
-        new TextFragment("    ", NOTE_D3 + 1),
-        new TextFragment("AEND", NOTE_F3),
-        new TextFragment("    ", NOTE_F3),
-        new TextFragment("PREY", NOTE_C3),
-        new TextFragment("    ", NOTE_C3),
-        new TextFragment("", NOTE_C3),
-        new TextFragment("", NOTE_C3),
-        new TextFragment("", NOTE_C3),
+        new TextFragment("GEHT", Note.NOTE_A3 + 1),
+        new TextFragment("    ", Note.NOTE_A3 + 1),
+        new TextFragment("DAWN", Note.NOTE_G3 + 1),
+        new TextFragment("    ", Note.NOTE_G3 + 1),
+        new TextFragment("AAN", Note.NOTE_G3),
+        new TextFragment("MAY", Note.NOTE_F3),
+        new TextFragment("   ", Note.NOTE_F3),
+        new TextFragment("NIYZ", Note.NOTE_D3 + 1),
+        new TextFragment("    ", Note.NOTE_D3 + 1),
+        new TextFragment("AEND", Note.NOTE_F3),
+        new TextFragment("    ", Note.NOTE_F3),
+        new TextFragment("PREY", Note.NOTE_C3),
+        new TextFragment("    ", Note.NOTE_C3),
+        new TextFragment("", Note.NOTE_C3),
+        new TextFragment("", Note.NOTE_C3),
+        new TextFragment("", Note.NOTE_C3),
         };
 
         // | 1     |       | 2     |       | 3     |       | 4     |       |
@@ -105,7 +99,16 @@ public class ExampleExternal08SAMSinging extends PApplet {
         mSAM.set_pitch(SAM.get_pitch_from_MIDI_note(mWords[mWordIndex].pitch));
         mSAM.say(mWords[mWordIndex].text, true);
 
-        Tone.note_on(pBeatCount % 2 == 0 ? NOTE_C2 : NOTE_C3, 50);
+        Tone.note_on(pBeatCount % 2 == 0 ? Note.NOTE_C2 : Note.NOTE_C3, 50);
+    }
+
+    public void mouseMoved() {
+        mSAM.set_mouth((int) map(mouseX, 0, width, 0, 255));
+        mSAM.set_throat((int) map(mouseY, 0, height, 0, 255));
+    }
+
+    public void mouseDragged() {
+        mSAM.set_speed((int) map(mouseX, 0, width, 0, 255));
     }
 
     public void audioblock(float[] pOutputSignal) {
