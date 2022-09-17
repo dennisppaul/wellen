31c4316f0645463a6e751c0b52f450a0d655ac4d
dennisppaul <dennis.paul@hfk-bremen.de>
2022-09-17 13:05:53 +0200

    added pitch detection

---
+++ PROJECTS-INCLUDED.md
@@ -0,0 +1,8 @@
+# Wellen / Projects + Sources Included in Wellen
+
+- [Electrosmith: DaisySP](https://github.com/electro-smith/DaisySP) ( MIT License )
+- [Joren Six: TarsosDSP](https://github.com/JorenSix/TarsosDSP) ( GPL-3.0 License )
+- [Josep Andreu + Ryan Billing: rakarrack](https://github.com/Stazed/rakarrack-plus) ( GPL-2.0 License )
+- [Gary P. Scavone: RtMidi](https://github.com/thestk/rtmidi)
+
+as well as more snippets + projects referenced in the header comments of source code files.
---
+++ src/wellen/examples/DSP/analysis/ExampleDSPAnalysis00PitchDetection.java
@@ -0,0 +1,67 @@
+package wellen.examples.DSP.analysis;
+
+import processing.core.PApplet;
+import wellen.DSP;
+import wellen.Wavetable;
+import wellen.Wellen;
+import wellen.analysis.PitchDetection;
+
+public class ExampleDSPAnalysis00PitchDetection extends PApplet {
+
+    /*
+     * this example demonstrates how to detect a pitch from an input signal and play the detected pitch back through
+     * an oscillator.
+     */
+
+    private final PitchDetection fPitchProcessor = new PitchDetection();
+    private final Wavetable fWavetable = new Wavetable();
+    private float fInputAmplification = 1.0f;
+
+    public void settings() {
+        size(640, 480);
+    }
+
+    public void setup() {
+        fWavetable.set_waveform(10, Wellen.WAVEFORM_SQUARE);
+        DSP.start(this, 1, 1);
+    }
+
+    public void mouseMoved() {
+        fInputAmplification = Wellen.clamp(map(mouseY, 0, height * 0.75f, 2.0f, 0.0f), 0, 2);
+    }
+
+    public void draw() {
+        background(255);
+        fill(0);
+        ellipse(width * 0.5f,
+                height * 0.5f,
+                fPitchProcessor.is_pitched() ? 100 : 5,
+                fPitchProcessor.is_pitched() ? 100 : 5);
+        DSP.draw_buffers(g, width, height);
+    }
+
+    public void audioblock(float[] pOutputSignal, float[] pInputSignal) {
+        /* amplify audio signal */
+        for (int i = 0; i < pInputSignal.length; i++) {
+            pInputSignal[i] *= fInputAmplification;
+        }
+
+        /* detect pitch and set oscillator */
+        fPitchProcessor.process(pInputSignal);
+        if (fPitchProcessor.is_pitched()) {
+            fWavetable.set_frequency(fPitchProcessor.get_pitch());
+            fWavetable.set_amplitude(0.25f, 32);
+        } else {
+            fWavetable.set_amplitude(0.0f, 32);
+        }
+
+        /* process output signal */
+        for (int i = 0; i < pOutputSignal.length; i++) {
+            pOutputSignal[i] = fWavetable.output();
+        }
+    }
+
+    public static void main(String[] args) {
+        PApplet.main(ExampleDSPAnalysis00PitchDetection.class.getName());
+    }
+}
